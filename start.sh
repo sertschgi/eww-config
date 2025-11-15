1dbe02a18b67b59c52b568329b53f7a0649ce82d
= <seppi@neureiter.bayern>
2025-11-15 14:41:55 +0100

    first

---
+++ start.sh
@@ -0,0 +1,9 @@
+eww open top-bar-window
+eww open right-bar-window
+eww open bottom-bar-window
+eww open left-bar-window
+
+eww open tl-corner
+eww open tr-corner
+eww open br-corner
+eww open bl-corner
